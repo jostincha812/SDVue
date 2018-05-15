d3f68fa280bc140b371ed3cddb21a292d71eb5b2
jostincha812 <jostin.cha812@gmail.com>
2018-05-15 21:43:10 +0200

    fix: no dash in library name

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    library: 'simple-directory-client-nuxt'
+    library: 'sdClientNuxt'
   }
 }
